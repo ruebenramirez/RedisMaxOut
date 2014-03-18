5bb20bc37d865c2b41aa7a179cade1bbfba6ea82
Rueben Ramirez <ruebenramirez@gmail.com>
2014-03-17 21:00:15 -0500

    max out your redis instance to tests memory handling

---
+++ redisMaxOut.py
@@ -0,0 +1,38 @@
+# maximize the key usage
+
+import redis
+import hashlib
+import datetime
+
+url = "<instanceName>.redistogo.com"
+port = <port>
+password = "<password>"
+
+def connect(url, port, password):
+    # r_server = redis.Redis("localhost")
+    r_server = redis.Redis(url,
+        port = port,
+        password = password)
+    return r_server
+
+def maxOut(r_server,
+        min = 0,
+        max = 900000,
+        valueMultiplier = 20,
+        printIter = 100):
+
+    for x in range(min, max):
+        m = hashlib.md5()
+        myDate = datetime.datetime.today()
+        m.update(str(myDate))
+        value = str(m.hexdigest()) * valueMutliplier
+        r_server.set(myDate, value)
+
+        if(x % printIter == 0):
+            print str(x) + ": " + str(r_server.get(x))
+
+            memoryUsed = r_server.info()['used_memory']
+            print "memory used: " + str(memoryUsed)
+
+r = connect(url, port, password)
+maxOut(r)
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get install python-dev
+
+pip install virtualenv
+pip install redis
+sudo git install guppy
+
