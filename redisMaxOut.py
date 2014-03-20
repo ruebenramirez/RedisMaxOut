d19077ce3ca57d840520d0658f816227d89b0105
Rueben Ramirez <ruebenramirez@gmail.com>
2014-03-19 23:01:17 -0500

    refactoring camel case to underscores for pep8

--- redisMaxOut.py
+++ redisMaxOut.py
@@ -8,36 +8,35 @@ import hashlib
 
 class MaxOutConfig():
     def __init__(self):
-        self.readConfig()
+        self.read_config()
     
-    def readConfig(self): 
+    def read_config(self): 
         try:
-            configFile = argv[1]
+            config_file = argv[1]
         except IndexError:
-            configFile = 'setup.cfg'
+            config_file = 'setup.cfg'
 
         config = ConfigParser.ConfigParser()
-        config.readfp(open(configFile))
+        config.readfp(open(config_file))
 
         # connection settings
-        self.url =  config.get('redis connection', 'url')
-        self.port = config.get('redis connection', 'port')
-        self.password = config.get('redis connection', 'password')
+        self.url =  str(config.get('redis connection', 'url'))
+        self.port = int(config.get('redis connection', 'port'))
+        self.password = str(config.get('redis connection', 'password'))
 
         # loop handler settings
         self.iterations = int(config.get('loop', 'iterations'))
-        self.valueMultiplier = int(config.get('loop', 'valueMultiplier'))
-        self.printIter = int(config.get('loop', 'printIter'))
+        self.value_multiplier = int(config.get('loop', 'value_multiplier'))
+        self.print_iter = int(config.get('loop', 'print_iter'))
 
-class RedisMaxOut():
+class MaxOut():
     def __init__(self, config):
-        # TODO if no config, then bail
         self.url = config.url
         self.port = config.port
         self.password = config.password
         self.iterations = config.iterations
-        self.valueMultiplier = config.valueMultiplier
-        self.printIter = config.printIter
+        self.value_multiplier = config.value_multiplier
+        self.print_iter = config.print_iter
         self.connect()
 
     def connect(self):
@@ -45,40 +44,34 @@ class RedisMaxOut():
             port = self.port,
             password = self.password)
 
-    def validateConfig(self):
+    def validate_config(self):
         # TODO check connection
         # TODO check loop config
 
         # FIXME stub
         return True
 
-    def flushOld(self):
+    def flush(self):
         self.r_server.flushall()
 
-    def maxOut(self):
-        if(not self.validateConfig()):
+    def max_out(self):
+        if(not self.validate_config()):
             print 'exiting...invalid configuration'
             return
 
         for x in range(0, self.iterations):
             m = hashlib.md5()
-            myDate = datetime.datetime.today()
-            m.update(str(myDate))
-            value = str(m.hexdigest()) * self.valueMultiplier
-            self.r_server.set(myDate, value)
+            my_date = datetime.datetime.today()
+            m.update(str(my_date))
+            value = str(m.hexdigest()) * self.value_multiplier
+            self.r_server.set(my_date, value)
 
-            if(x % self.printIter == 0):
-                print str(x) + ": " + str(self.r_server.get(x))
+            if(x % self.print_iter == 0):
 
-                redisMemoryUsed = self.r_server.info()['used_memory']
-                print "memory used: " + str(redisMemoryUsed)
+                redis_memory_used = self.r_server.info()['used_memory']
+                print str(x) + ": " + str(redis_memory_used) + " bytes"
 
-maxOutConfig = MaxOutConfig()
-redisTorture = RedisMaxOut(maxOutConfig)
-redisTorture.flushOld()
-redisTorture.maxOut()
-
-# def main():
-
-# if __name__ == "__main__:":
-    # main()
+max_out_config = MaxOutConfig()
+redisTorture = MaxOut(max_out_config)
+redisTorture.flush()
+redisTorture.max_out()
